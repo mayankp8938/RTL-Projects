5e3262ef5218e53f20114e8b5fd7a939010aaf81
Mayank <66165652+mayankp8938@users.noreply.github.com>
2025-05-04 15:45:54 +0800

    HA

---
+++ HalfAdder/HA_tb.v
@@ -0,0 +1,18 @@
+`timescale 1ns/1ps
+module HA_tb();
+reg A,B;
+wire Carry,Sum;
+
+HA g1 (A,B,Carry,Sum);
+
+initial begin 
+
+
+A = 1'b0;B = 1'b0;
+#5 A = 1'b0;B = 1'b1;
+#5 A = 1'b1;B = 1'b0;
+#5 A = 1'b1;B = 1'b1;
+
+#5 $finish;
+end 
+endmodule 
